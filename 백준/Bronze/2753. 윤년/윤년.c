61a8980ac7c9d805ebba1cca0ab0114215837b04
mguny <mguny0203@gmail.com>
2026-02-01 10:11:28 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    
+    int year;
+    
+    scanf("%d", &year);
+    
+    if((year%4 == 0) && (year%400 == 0))
+        printf("1\n");
+    else if((year%4 ==0) && (year%100 != 0))
+        printf("1\n");
+    else
+        printf("0");
+    
+    return 0;
+}
